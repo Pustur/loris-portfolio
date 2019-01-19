020ec221011504b0058eba0c50b2c60939bac99b
Loris Bettazza <loris.bettazza@gmail.com>
2019-01-19 01:25:11 +0100

    :construction: Fix quotes in config and simplify layout and seo components

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,18 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => (
-  <div
-    style={{
-      margin: `0 auto`,
-      maxWidth: 960,
-      padding: `0px 1.0875rem 1.45rem`,
-      paddingTop: 0,
-    }}
-  >
+  <div>
     {children}
     <footer>
-      © {new Date().getFullYear()}, Built with
-      {` `}
+      © {new Date().getFullYear()}, Built with{' '}
       <a href="https://www.gatsbyjs.org">Gatsby</a>
     </footer>
   </div>
