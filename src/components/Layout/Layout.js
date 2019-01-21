9b4e0632b24c0bbf656796797b55243a1746a8fc
Loris Bettazza <loris.bettazza@gmail.com>
2019-01-21 17:07:12 +0100

    :lipstick: Add global styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
+import { colors, fonts } from '../../utils/variables';
 
 import Menu from '../Menu/Menu';
 import Footer from '../Footer/Footer';
 
+const GlobalStyles = createGlobalStyle`
+  body {
+    color: ${colors.foreground};
+    background-color: ${colors.background};
+    font-family: ${fonts.base};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  h1,
+  h2,
+  h3 {
+    color: ${colors.foregroundLight};
+    font-family: ${fonts.headings};
+  }
+
+  a {
+    color: ${colors.accent};
+    text-decoration: none;
+  }
+
+  b,
+  strong {
+    color: ${colors.foregroundLight};
+  }
+`;
+
 const Layout = ({ children }) => (
-  <div>
+  <>
+    <GlobalStyles />
     <header>
       <nav>
         <Menu />
@@ -13,7 +42,7 @@ const Layout = ({ children }) => (
     </header>
     <main>{children}</main>
     <Footer />
-  </div>
+  </>
 );
 
 Layout.propTypes = {
--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,6 +1,14 @@
 const colors = {
+  foreground: '#d4d4e8',
   foregroundLight: '#fff',
+  background: '#1c1c2e',
   accent: '#ee0a96',
 };
 
-export { colors };
+const fonts = {
+  base:
+    '"Europa", "Avenir", "-apple-system", "Helvetica Neue", "Arial", sans-serif',
+  headings: '"Streamster", "Blacksword", sans-serif',
+};
+
+export { colors, fonts };
