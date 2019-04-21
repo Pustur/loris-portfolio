6fc86a06e9af4d29100d7214a353584203cf79c6
Loris Bettazza <loris.bettazza@gmail.com>
2019-04-21 16:47:41 +0200

    :bug: Add home link text for screen readers

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -87,6 +87,17 @@ const GlobalStyles = createGlobalStyle`
     }
   }
 
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
+
   @media(${mediaQueries.mdMin}){
     html {
       font-size: 18px;
