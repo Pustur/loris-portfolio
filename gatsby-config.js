67e24bb73a017a3c7b27ed7fd892e558337f0099
Loris Bettazza <loris.bettazza@gmail.com>
2019-04-21 16:35:29 +0200

    :bento: Add custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Loris Bettazza Portfolio',
+        short_name: 'Loris Bettazza',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#1c1c2e',
+        theme_color: '#1c1c2e',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-eslint',
